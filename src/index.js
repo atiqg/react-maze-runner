f06de025c1ed99681318d4ae117dcb809182ad72
AtiqGauri <gauriatiq@gmail.com>
2020-11-05 23:20:40 +0530

    feat: basic global maze state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import ReactDOM from 'react-dom';
 import './index.css';
+import React, { useState, useLayoutEffect } from 'react';
 import { getInput, randomPositions } from './utils';
 import Maze from './mazeGenerator';
 
-let input = getInput();
+import GlobalState from './globalMazeData';
+
+//let input = getInput();
+let input = {'x':10, 'y':10};
 
 const [randomFoods, centreMario] = randomPositions(parseInt(input.x), parseInt(input.y));
 
 function Game() {
+  const [mazeData, setMazeData] = useState({});
+
+  useLayoutEffect(() => {
+    setMazeData(mazeData => ({...mazeData, test: "passed"}));
+  }, []);
+
   return (
     <div className="game">
       <div className="game-maze">
-        <Maze x={input.x} y={input.y} foodLoc={randomFoods} marioLoc={centreMario} />
+        <GlobalState.Provider value={[mazeData, setMazeData]}>
+          <Maze x={input.x} y={input.y} foodLoc={randomFoods} marioLoc={centreMario} />
+        </GlobalState.Provider>
       </div>
       <div className="game-info">
       </div>
--- src/mazeGenerator.js
+++ src/mazeGenerator.js
@@ -1,45 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect, useContext } from 'react';
 import food_img from './assets/food.svg';
 import mario_img from './assets/mario.svg';
+import GlobalState from './globalMazeData';
 
 export default function Maze(props) {
+  const [mazeData, setMazeData] = useContext(GlobalState);
 
-    const renderBlock = (blockCount)=> {
+  console.log(mazeData.test);
 
-      if(props.foodLoc.indexOf(blockCount) !== -1){
-        return <Block value={blockCount} image={`url(${food_img})`} />;
-      }else if(props.marioLoc === blockCount){
-        return <Block value={blockCount} image={`url(${mario_img})`} />;
-      }else{
-        return <Block value={blockCount} image={'none'} />;
-      }
+  const renderBlock = (blockCount)=> {
 
+    if(props.foodLoc.indexOf(blockCount) !== -1){
+      return <Block value={blockCount} image={`url(${food_img})`} />;
+    }else if(props.marioLoc === blockCount){
+      return <Block value={blockCount} image={`url(${mario_img})`} />;
+    }else{
+      return <Block value={blockCount} image={'none'} />;
     }
-  
-    const addColumn = (row) => {
-      let column = [];
-      for(let i=0; i<props.x; i++ ){
-        column.push(renderBlock(props.x*row + (i + 1)));
-      }
-      return column;
+
+  }
+
+  const addColumn = (row) => {
+    let column = [];
+    for(let i=0; i<props.x; i++ ){
+      column.push(renderBlock(props.x*row + (i + 1)));
     }
-  
-    const addRows = () =>{
-      let rows = [];
-      for(let i=0; i<props.y; i++){
-        rows.push(<div className="maze-row">{addColumn(i)}</div>);
-      }
-      return rows;
+    return column;
+  }
+
+  const addRows = () =>{
+    let rows = [];
+    for(let i=0; i<props.y; i++){
+      rows.push(<div className="maze-row">{addColumn(i)}</div>);
     }
-    
-    const status = 'MAZE RUNNER';
+    return rows;
+  }
   
-    return (
-      <div>
-        <div className="status">{status}</div>
-        {addRows()}
-      </div>
-    );
+  const status = 'MAZE RUNNER';
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      {addRows()}
+    </div>
+  );
 }
 
 function Block(props) {
